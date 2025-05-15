1934e2be516343c3efad8ec7605ce4f35647fd4d
Andre <andre@Smoothie.local>
2025-05-15 21:47:22 +0700

    tugas-hari-14

---
+++ components/ui/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="bg-sky-500 dark:bg-gray-800 shadow-md p-5">
+      <div className="max-w-xl px-6">
+        <h1 className="text-lg font-bold">ShareLink</h1>
+      </div>
+    </header>
+  )
+}
